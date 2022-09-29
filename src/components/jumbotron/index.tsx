33d9770898177e7d23c8cd8e4d1534cce2318e2a
Vitor Albarado <vitor.jardina@gmail.com>
2022-09-28 23:53:46 -0400

    adicionando componente jumbotron

---
+++ src/components/jumbotron/index.tsx
@@ -0,0 +1,23 @@
+import Badge from 'react-bootstrap/Badge';
+import './jumbotron.css'
+
+export default function Jumbotron() {
+    return(
+            <section className="jumbotron">
+                <div className="container">
+                <h1 className="display-4">Mais de 10 mil empréstimos realizados</h1>
+                <p className="lead">Acervo com mais de 25 mil livros das mais variadas áreas da educação disponiveis para todos os alunos. <br/>
+                    Cadastre-se, escolha o livro e boa leitura!
+                    </p>
+                    <article className="jumbrtron-genero">
+                        <Badge bg="secondary">Biologia</Badge>{' '}
+                        <Badge bg="secondary">Filosofia</Badge>{' '}
+                        <Badge bg="secondary">Computação</Badge>{' '}
+                        <Badge bg="secondary">Jornalismo</Badge>{' '}
+                        <Badge bg="secondary">Saúde</Badge>{' '}
+                        <Badge bg="secondary">Moda</Badge>{' '}
+                    </article>
+                </div>
+            </section>
+    )
+}
