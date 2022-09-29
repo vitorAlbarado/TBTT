dd70688f57fea3c280c596ffc2c7f2b3a1589747
Vitor Albarado <vitor.jardina@gmail.com>
2022-09-29 14:27:21 -0400

    adicionando link de rota e estilizando

--- src/components/cabecalho/index.tsx
+++ src/components/cabecalho/index.tsx
@@ -1,19 +1,19 @@
 import   './cabecalho.css'
 import logo from '../../assets/img/logo.png'
+import { Link } from "react-router-dom";
 
 export default function Cabecalho(){
-	
     return(
         <header>
 			<section className='cabecalho'>
 			<h1><a href="#"><img src={logo} style={{width:"150px"}} alt="Logo da Empresa"/></a></h1>
 				<nav className='menu'>
 					<ul>
-						<li><a href="../home.html">Home</a></li>
-						<li><a href="./emprestar.html" id="emprestar">Emprestar</a></li>
-						<li><a href="./cadastrar.html">Cadastrar</a></li>
-						<li><a href="./acervo.html">Acervo</a></li>
-						<li><a href="./historico.html">Historico</a></li>
+						<li><Link to="/">Home</Link></li>
+						<li><Link to="/emprestar" >Emprestar</Link></li>						
+						<li><Link to="./cadastrar.html" >Cadastrar</Link></li>
+						<li><Link to="./acervo.html" >Acervo</Link></li>
+						<li><Link to="./historico.html">Historico</Link></li>
 					</ul>
 				</nav>
 			</section>
