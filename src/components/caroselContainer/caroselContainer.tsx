e2d49189a500ad24264c26d810503cabf175cf83
Vitor Albarado <vitor.jardina@gmail.com>
2022-09-29 11:54:54 -0400

    criando objeto livro e renderizando na tela

--- src/components/caroselContainer/caroselContainer.tsx
+++ src/components/caroselContainer/caroselContainer.tsx
@@ -6,102 +6,71 @@ import Lideranca from '../../assets/img/Liderança.png'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 
+export interface livro {
+    titulo: string,
+    src:string,
+    descricao: string,
+    genero?:string,
+    qtdEmprestimos: number
+}
+const Livro: Array<livro>=[
+    {titulo: 'Guia Front End',
+        src: GuiaFrontEnd,
+        descricao: 'O melhor e mais completo guia de font-end do mercado do zero ao pro.',
+        qtdEmprestimos: 5
+    },
+    {titulo: 'ApacheKafka',
+        src: ApacheKafka,
+        descricao: 'O melhor e mais completo guia de ApacheKafka do mercado do zero ao pro.',
+        qtdEmprestimos: 9
+    },
+    {titulo: 'javascript',
+        src: javascript,
+        descricao: 'O melhor e mais completo guia de javascript do mercado do zero ao pro.',
+        qtdEmprestimos: 3
+    },
+    {titulo: 'Lideranca',
+        src: Lideranca,
+        descricao: 'O melhor e mais completo guia de Lideranca do mercado do zero ao pro.',
+        qtdEmprestimos: 6
+    }
+]
+
 
 export default function CaroselContainer(){
+    
     return(
         <section className="caroselContainer">
 			<h2 className="container-secoes__titulo">Mais Populares</h2>
 			<section className="container-secoes">
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img className="img-carrosel" variant="top" src={javascript} />
+                {Livro.map((item)=>(
+                    <Card style={{ width: '18rem' }}>
+                    <Card.Img className="img-carrosel" variant="top" src={item.src} />
                     <Card.Body>
-                        <Card.Title>Guia Front-end</Card.Title>
+                        <Card.Title>{item.titulo}</Card.Title>
                             <Card.Text>
-                                O melhor e mais completo guia de font-end do mercado do zero ao pro.
+                                {item.descricao}
                             </Card.Text>
-                        <Button variant="primary">Emprestar</Button>
+                        <Button  variant="primary">Emprestar</Button>
                     </Card.Body>
                 </Card>
-
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img className="img-carrosel" variant="top" src={Lideranca} />
-                    <Card.Body>
-                        <Card.Title>Guia Front-end</Card.Title>
-                            <Card.Text>
-                                O melhor e mais completo guia de font-end do mercado do zero ao pro.
-                            </Card.Text>
-                        <Button variant="primary">Emprestar</Button>
-                    </Card.Body>
-                </Card>
-
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img className="img-carrosel" variant="top" src={ApacheKafka} />
-                    <Card.Body>
-                        <Card.Title>Guia Front-end</Card.Title>
-                            <Card.Text>
-                                O melhor e mais completo guia de font-end do mercado do zero ao pro.
-                            </Card.Text>
-                        <Button variant="primary">Emprestar</Button>
-                    </Card.Body>
-                </Card>
-
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img className="img-carrosel" variant="top" src={GuiaFrontEnd} />
-                    <Card.Body>
-                        <Card.Title>Guia Front-end</Card.Title>
-                            <Card.Text>
-                                O melhor e mais completo guia de font-end do mercado do zero ao pro.
-                            </Card.Text>
-                        <Button variant="primary">Emprestar</Button>
-                    </Card.Body>
-                </Card>
+                ))}
             </section> 
 
             <h2 className="container-secoes__titulo">Ciência e Matemática</h2>
 			<section className="container-secoes">
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img className="img-carrosel" variant="top" src={Lideranca} />
-                    <Card.Body>
-                        <Card.Title>Guia Front-end</Card.Title>
-                            <Card.Text>
-                                O melhor e mais completo guia de font-end do mercado do zero ao pro.
-                            </Card.Text>
-                        <Button variant="primary">Emprestar</Button>
-                    </Card.Body>
-                </Card>
-
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img className="img-carrosel" variant="top" src={ApacheKafka} />
-                    <Card.Body>
-                        <Card.Title>Guia Front-end</Card.Title>
-                            <Card.Text>
-                                O melhor e mais completo guia de font-end do mercado do zero ao pro.
-                            </Card.Text>
-                        <Button variant="primary">Emprestar</Button>
-                    </Card.Body>
-                </Card>
-
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img className="img-carrosel" variant="top" src={javascript} />
-                    <Card.Body>
-                        <Card.Title>Guia Front-end</Card.Title>
-                            <Card.Text>
-                                O melhor e mais completo guia de font-end do mercado do zero ao pro.
-                            </Card.Text>
-                        <Button variant="primary">Emprestar</Button>
-                    </Card.Body>
-                </Card>
-
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img className="img-carrosel" variant="top" src={GuiaFrontEnd} />
-                    <Card.Body>
-                        <Card.Title>Guia Front-end</Card.Title>
-                            <Card.Text>
-                                O melhor e mais completo guia de font-end do mercado do zero ao pro.
-                            </Card.Text>
-                        <Button variant="primary">Emprestar</Button>
-                    </Card.Body>
-                </Card>
+                {Livro.map((item)=>(
+                        <Card style={{ width: '18rem' }}>
+                        <Card.Img className="img-carrosel" variant="top" src={item.src} />
+                        <Card.Body>
+                            <Card.Title>{item.titulo}</Card.Title>
+                                <Card.Text>
+                                    {item.descricao}
+                                </Card.Text>
+                            <Button  variant="primary">Emprestar</Button>
+                        </Card.Body>
+                    </Card>
+                    ))}    
             </section> 
 	    </section>
     )
