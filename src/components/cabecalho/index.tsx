ec4a647f3d2c3553c70d44e0019de13f22172eb0
Vitor Albarado <vitor.jardina@gmail.com>
2022-09-30 14:24:11 -0400

    adicionando rotas para novas paginas

--- src/components/cabecalho/index.tsx
+++ src/components/cabecalho/index.tsx
@@ -2,18 +2,22 @@ import   './cabecalho.css'
 import logo from '../../assets/img/logo.png'
 import { Link } from "react-router-dom";
 
-export default function Cabecalho(){
+interface Props{
+	ativo?:string
+}
+export default function Cabecalho(porps:Props){
+
     return(
         <header>
 			<section className='cabecalho'>
 			<h1><a href="#"><img src={logo} style={{width:"150px"}} alt="Logo da Empresa"/></a></h1>
 				<nav className='menu'>
 					<ul>
-						<li><Link to="/">Home</Link></li>
-						<li><Link to="/emprestar" >Emprestar</Link></li>						
-						<li><Link to="./cadastrar.html" >Cadastrar</Link></li>
-						<li><Link to="./acervo.html" >Acervo</Link></li>
-						<li><Link to="./historico.html">Historico</Link></li>
+						<li><Link to="/" id={porps.ativo ==='home' ? porps.ativo:''}>Home</Link></li>
+						<li><Link to="/emprestar" id={porps.ativo ==='emprestar' ? porps.ativo:''}>Emprestar</Link></li>						
+						<li><Link to="/cadastrar" id={porps.ativo ==='cadastrar' ? porps.ativo:''}>Cadastrar</Link></li>
+						<li><Link to="/acervo" id={porps.ativo ==='acervo' ? porps.ativo:''}>Acervo</Link></li>
+						<li><Link to="/historico"id={porps.ativo ==='historico' ? porps.ativo:''}>Historico</Link></li>
 					</ul>
 				</nav>
 			</section>
