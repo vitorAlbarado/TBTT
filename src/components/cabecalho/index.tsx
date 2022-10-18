fbf04a39692e36468f61d587e3a7dcfba79715ec
Vitor Albarado <vitor.jardina@gmail.com>
2022-10-18 16:49:41 -0400

    Adicionando lib css module e sass e refatorando

--- src/components/cabecalho/index.tsx
+++ src/components/cabecalho/index.tsx
@@ -1,4 +1,4 @@
-import   './cabecalho.css'
+import style from './Cabecalho.module.scss';
 import logo from '../../assets/img/logo.png'
 import { Link } from "react-router-dom";
 
@@ -8,16 +8,16 @@ interface Props{
 export default function Cabecalho(porps:Props){
 
     return(
-        <header>
-			<section className='cabecalho'>
+        <header className={style.header}>
+			<section className={style.header__cabecalho}>
 			<h1><a href="#"><img src={logo} style={{width:"150px"}} alt="Logo da Empresa"/></a></h1>
-				<nav className='menu'>
-					<ul>
-						<li><Link to="/" id={porps.ativo ==='home' ? porps.ativo:''}>Home</Link></li>
-						<li><Link to="/emprestar" id={porps.ativo ==='emprestar' ? porps.ativo:''}>Emprestar</Link></li>						
-						<li><Link to="/cadastrar" id={porps.ativo ==='cadastrar' ? porps.ativo:''}>Cadastrar</Link></li>
-						<li><Link to="/acervo" id={porps.ativo ==='acervo' ? porps.ativo:''}>Acervo</Link></li>
-						<li><Link to="/historico"id={porps.ativo ==='historico' ? porps.ativo:''}>Historico</Link></li>
+				<nav className={style.menu}>
+					<ul className={style.menu__list}>
+						<li className={style.menu__link}><Link to="/" id={porps.ativo ==='home' ? porps.ativo:''}>Home</Link></li>
+						<li className={style.menu__link}><Link to="/emprestar" id={porps.ativo ==='emprestar' ? porps.ativo:''}>Emprestar</Link></li>						
+						<li className={style.menu__link}><Link to="/cadastrar" id={porps.ativo ==='cadastrar' ? porps.ativo:''}>Cadastrar</Link></li>
+						<li className={style.menu__link}><Link to="/acervo" id={porps.ativo ==='acervo' ? porps.ativo:''}>Acervo</Link></li>
+						<li className={style.menu__link}><Link to="/historico"id={porps.ativo ==='historico' ? porps.ativo:''}>Historico</Link></li>
 					</ul>
 				</nav>
 			</section>
