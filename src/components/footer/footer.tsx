4a32caa2547f112cf33e4b01e93c700e9d3c079e
Vitor Albarado <vitor.jardina@gmail.com>
2022-10-20 12:59:46 -0400

    refatorando style do footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.scss';
 export default function Footer(){
     return(
         <footer className={styles.footer}>
-	        <img src={logo} style={{height:"100px"}} alt="logo"/>
+	        <img src={logo}  alt="logo"/>
 	        <p>Copyright vitor</p>
         </footer>
     )
