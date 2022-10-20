1d40482e30a490d5f4156a2d8891c93893d0416c
Vitor Albarado <vitor.jardina@gmail.com>
2022-10-20 13:04:10 -0400

    adionando normalize css

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-//import './index.css';
-import App from './pages/App';
+//import 'normalize.css';
+import './index.css';
+import Rotas from 'pages/rotas';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Rotas />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
