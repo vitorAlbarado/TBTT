ab8aa13e6f051d6ef2d4a9ce629a06329e59ca17
vitor <vitor.jardina@gmail.com>
2022-11-03 13:08:23 -0400

    refatorando interfaces

---
+++ src/Interfaces/IEmprestimos.ts
@@ -0,0 +1,11 @@
+export default interface IEmprestimos  {
+  idAluno:number,
+  name:string,
+  idLivro:string,
+  turmaAluno:string,
+  tituloLivro:string,
+  editora?:string,
+  dataLancamento?:Date,
+  autor?:string,
+};
+
---
+++ src/Interfaces/ILivros.ts
@@ -0,0 +1,7 @@
+export default interface ILivros {
+  idLivro:string,
+  tituloLivro:string,
+  editora?:string,
+  dataLancamento?:Date,
+  autor?:string,
+}
