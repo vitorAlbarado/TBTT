a713979a36a99651a5638f61a112a498e2d8d7b8
Vitor Albarado <vitor.jardina@gmail.com>
2022-11-03 21:43:48 -0400

    Implementando requisição com axios

--- src/pages/acervo/acervo.tsx
+++ src/pages/acervo/acervo.tsx
@@ -1,34 +1,46 @@
 import styles from './Acervo.module.scss';
+import axios from 'axios';
+import IEmprestimos from 'Interfaces/IEmprestimos';
+import { useEffect, useState } from 'react';
+import ILivros from 'Interfaces/ILivros';
 
-export default function Acervo(){
-    return (
-        <section className={styles.container}>
-		    <input className={styles.container__input} id="pesquisar__input" type="text" placeholder="Informe o ID"/>
-		    <input className={styles.container__pesquisar} type="submit" value="Pesquisar"/>
+
+export default function Acervo() {
+	const [acervo, setAcervo] = useState<ILivros[]>([]);
+	const [busca, setBusca] = useState(false);
+
+	if(busca){
+			axios.get<ILivros[]>('https://jsonurl.com/XUO1')
+			.then(resposta => setAcervo(resposta.data))
+			.catch(() => alert("Não foi possivel buscar os dados"))
+	}
+
+	
+
+	return (
+		<section className={styles.container}>
+			<input className={styles.container__input}  id="pesquisar__input" type="text" placeholder="Informe o ID" />
+			<button className={styles.container__pesquisar} type="submit" onClick={()=>setBusca(!busca)}>Pesquisar</button>
 			<table className={styles.container__tabela}>
 				<thead className={styles.container__tabela__thead}>
-						<tr>
+
+					<tr>
 						<th >ID Livro</th>
 						<th >Titulo Livro</th>
-						<th >Autor</th>
 						<th >Editora</th>
-						</tr>
+						<th >Autor</th>
+					</tr>
 				</thead>
 				<tbody className="tabela-historico__body">
-					<tr>
-						<td>texto</td>
-						<td>texto</td>
-						<td>texto</td>
-						<td>texto</td>
+					{acervo.map((item, index) => <tr key={index}>
+						<td>{item.idLivro}</td>
+						<td>{item.tituloLivro}</td>
+						<td>{item.editora}</td>
+						<td>{item.autor}</td>
 					</tr>
-					<tr>
-						<td>texto</td>
-						<td>texto</td>
-						<td>texto</td>
-						<td>texto</td>
-					</tr>
+					)}
 				</tbody>
 			</table>
-	</section>
-    )
+		</section>
+	)
 }
--- src/pages/historico/historico.tsx
+++ src/pages/historico/historico.tsx
@@ -1,33 +1,42 @@
+import axios from 'axios';
+import IEmprestimos from 'Interfaces/IEmprestimos';
+import { useEffect, useState } from 'react';
 import styles from '../acervo/Acervo.module.scss';
-export default function Historico(){
-    return(
-        <section className={styles.container}>
-		    <input className={styles.container__input} id="pesquisar__input" type="text" placeholder="Informe o ID"/>
-		    <input className={styles.container__pesquisar} type="submit" value="Pesquisar"/>
+
+export default function Historico() {
+	const [emprestimos, setEmprestimos] = useState<IEmprestimos[]>([])
+
+	useEffect(() => {
+		axios.get<IEmprestimos[]>('https://jsonurl.com/_sO2')
+			.then((resposta) => {
+				setEmprestimos(resposta.data)
+			})
+	}, [])
+
+	return (
+		<section className={styles.container}>
+			<input className={styles.container__input} id="pesquisar__input" type="text" placeholder="Informe o ID" />
+			<input className={styles.container__pesquisar} type="submit" value="Pesquisar" />
 			<table className={styles.container__tabela}>
 				<thead className={styles.container__tabela__thead}>
 					<tr>
+						<th >Matricula</th>
+						<th >Aluno</th>
+						<th >Turma</th>
 						<th >ID Livro</th>
-						<th >Titulo Livro</th>
-						<th >Autor</th>
-						<th >Editora</th>
+						<th >Titulo</th>
 					</tr>
 				</thead>
 				<tbody className="tabela-historico__body">
-					<tr>
-						<td>texto</td>
-						<td>texto</td>
-						<td>texto</td>
-						<td>texto</td>
-					</tr>
-					<tr>
-						<td>texto</td>
-						<td>texto</td>
-						<td>texto</td>
-						<td>texto</td>
-					</tr>
+					{emprestimos.map((item, index) => <tr key={index}>
+						<td>{item.matriculaAluno}</td>
+						<td>{item.nomeAluno}</td>
+						<td>{item.turmaAluno}</td>
+						<td>{item.idLivro}</td>
+						<td>{item.tituloLivro}</td>
+					</tr>)}
 				</tbody>
 			</table>
-	</section>
-    )
+		</section>
+	)
 }
