830322b7dceecdffb7f6722418d45a83d8d66f78
Vitor Albarado da Silva <vitor.silva@inventuspower.com>
2023-05-09 03:33:55 -0400

    add axios get livros

--- src/pages/acervo/acervo.tsx
+++ src/pages/acervo/acervo.tsx
@@ -1,42 +1,46 @@
 import styles from './Acervo.module.scss';
 import axios from 'axios';
-import IEmprestimos from 'Interfaces/IEmprestimos';
-import { useEffect, useState } from 'react';
-import ILivros from 'Interfaces/ILivros';
 
+import { useEffect, useState } from 'react';
+
+import { type } from 'os';
+
+type ILivros ={
+	titulo?:string,
+	autor?:string,
+	genero?:string,
+  }
 
 export default function Acervo() {
 	const [acervo, setAcervo] = useState<ILivros[]>([]);
 	const [busca, setBusca] = useState(false);
 
-	if(busca){
-			axios.get<ILivros[]>('https://jsonurl.com/XUO1')
-			.then(resposta => setAcervo(resposta.data))
+	 function buscaLivros(){
+			axios.get('http://localhost:8080/livros')
+			.then(resposta => setAcervo(resposta.data.content))
 			.catch(() => alert("Não foi possivel buscar os dados"))
-	}
-
-	
+		}
+		
+		
 
 	return (
 		<section className={styles.container}>
 			<input className={styles.container__input}  id="pesquisar__input" type="text" placeholder="Informe o ID" />
-			<button className={styles.container__pesquisar} type="submit" onClick={()=>setBusca(!busca)}>Pesquisar</button>
+			<button className={styles.container__pesquisar} type="submit" onClick={()=>buscaLivros()}>Pesquisar</button>
 			<table className={styles.container__tabela}>
 				<thead className={styles.container__tabela__thead}>
 
 					<tr>
-						<th >ID Livro</th>
 						<th >Titulo Livro</th>
-						<th >Editora</th>
 						<th >Autor</th>
+						<th >Genero</th>
 					</tr>
 				</thead>
 				<tbody className="tabela-historico__body">
-					{acervo.map((item, index) => <tr key={index}>
-						<td>{item.idLivro}</td>
-						<td>{item.tituloLivro}</td>
-						<td>{item.editora}</td>
+				    {acervo.map((item, index) => <tr key={index}>
+						<td>{item.titulo}</td>
 						<td>{item.autor}</td>
+						<td>{item.genero}</td>
 					</tr>
 					)}
 				</tbody>
